a5b74b2d2775f56fe797a9b2af4d957d52a428d8
Pavel Nichita <pnichita@ull.edu.es>
2019-02-26 08:28:19 +0000

    -Add usage info (kind of)

--- unpack-deb.sh
+++ unpack-deb.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+echo "./unpack-deb.sh <deb directory> <unpack directory>"
+echo "Where"
+echo "<deb directory> - is a directory with .deb packages."
+echo "<unpack directory> - is a directory where previous .deb packages"
+echo "                     will be unpacked. This directory MUST exist."
+echo "ex: ./unpack-deb.sh deb-packages/ deb-unpacked/"
+echo "===================================================================="
+echo
+
 set -ex
 
 DEBFILESDIR=$1
 EXTRACTDIR=$2
-
 
 for f in $(ls -1 $DEBFILESDIR | grep .deb)
 do
     dpkg-deb -x $DEBFILESDIR$f $EXTRACTDIR
     dpkg-deb -e $DEBFILESDIR$f $EXTRACTDIR
 done
+
