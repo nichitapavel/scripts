07ee33e647c95efa2f8b2e687a63b34fa06785a8
Pavel Nichita <pnichita@ull.edu.es>
2019-03-25 09:28:02 +0000

    -Remove saving and parsing pm_info data, add custom loops and better naming for files

--- metrics.sh
+++ metrics.sh
@@ -2,8 +2,11 @@
 
 # set -ex
 
-REMOTE_USER=$1
-HOST=$2
+# TODO Add a proper arguments parser
+
+# REMOTE_USER=$1
+# HOST=$2
+LOOPS=$1
 
 
 PMLIB=PMLib
@@ -15,6 +18,8 @@ CAPEW=watt-APCape.json
 CAPEV=volt-APCape.json
 CAPEA=amp-APCape.json
 
+MATRIX_SIZE=(100 200 300 400 500 600 700 800 900 1000)
+
 PMLIB_WATT=${PMLIB}-${WATT}
 PMLIB_VOLT=${PMLIB}-${VOLT}
 PMLIB_AMP=${PMLIB}-${AMP}
@@ -23,26 +28,45 @@ PM_INFO_WATT=${PM_INFO}-${WATT}
 PM_INFO_VOLT=${PM_INFO}-${VOLT}
 PM_INFO_AMP=${PM_INFO}-${AMP}
 
+DIRECTORY=~/data/odroid-ubuntu-model-a/2019-03-22-09-00-float-data/
+SLEEP_PMLIB_STARTUP=5
+SLEEP_START=5
+SLEEP_FINISH=5
+
 
 #############################################################################################################
 echo "**************** ${PMLIB_WATT}"
 
-for i in $(seq 1 10);
+for i in $(seq 1 ${LOOPS});
 do
-    echo "============== Run: ${i} =============="
-
-    screen -dmS ${PMLIB_WATT} /home/debian/bin/pmlib_server --configfile git/PMLib/new/${CAPEW}
-    screen -h 1000000000 -L -dmS ${PM_INFO_WATT} /home/debian/bin/pm-info -s localhost:6526 -r APCape8L
-    sleep 20s
+  echo "============== Run: ${i} =============="
+  for j in ${MATRIX_SIZE[@]}
+  do
+    echo "============== Size: ${j} =============="
+    ssh debian@10.209.2.79 "screen -dmS ${PMLIB_WATT} ~/bin/pmlib_server --configfile ~/git/PMLib/new/${CAPEW}" &> /dev/null
+    sleep ${SLEEP_PMLIB_STARTUP}s
+    # screen -h 1000000000 -L -dmS ${PM_INFO_WATT} ~/bin/pm-info -s localhost:6526 -r APCape8L
+    if [ ${i} -lt 10 ]; then
+      I=0${i}
+    else I=${i}
+    fi
+    if [ "${j}" -lt "1000" ]; then
+      J=0${j}
+    else J=${j}
+    fi
+    screen -dmS ${PM_INFO_WATT} ~/git/PMLib/Python/.venv/bin/python ~/git/PMLib/Python/client/thread_flask_pminfo.py -s 10.209.2.79:6526 -d ${DIRECTORY} -f data-${J}-${I}.csv -r APCape8L
+    ##############################
+    sleep ${SLEEP_START}s
 
     START=$(date +%Y-%m-%d\ %H:%M:%S\ %N)
 
     # matrix multiplication linux odroid
-    ssh ${REMOTE_USER}@${HOST} "/home/odroid/matrix-jar-app-0.2.1/bin/matrix-jar-app 1000 50 0"
+    ssh odroid@10.209.2.95 "~/matrix-jar-app-0.4.1/bin/matrix-jar-app ${j} 50 false http://10.209.3.126:5000/message"
 
     # matrix multiplication android odroid
-    # cd matrix-android-appium-0.1/bin/
-    # JAVA_HOME=/opt/jdk1.8.0_202/jre ./matrix-android-appium 1000 50 false
+    # adb connect 10.209.2.95:5555 &> /dev/null
+    # cd development/releases/appium/matrix-android-appium-0.2.1/bin/ > /dev/null
+    # ./matrix-android-appium ${j} 50 false  http://10.209.3.126:5000/message
     # cd - > /dev/null
 
     FINISH=$(date +%Y-%m-%d\ %H:%M:%S\ %N)
@@ -50,71 +74,19 @@ do
     echo "Start matrix multiplication: ${START}"
     echo -e "Finish matrix multiplication: ${FINISH}\n"
 
-    sleep 60s
-    screen -X -S ${PM_INFO_WATT} quit
-    screen -X -S ${PMLIB_WATT} quit
-    awk '{print $1":"$2":"$3}' screenlog.0 | awk -F ':' '{print $1":"$2":"$3","$5}' > ${PM_INFO_WATT}.date.data.${i}
-    awk '{print $1":"$2":"$3}' screenlog.0 | awk -F ':' '{print $5}' > ${PM_INFO_WATT}.data.${i}
-    echo "Time,Power(Watt)" > header1.txt
-    echo "Power(Watt)" > header2.txt
-    cat header1.txt ${PM_INFO_WATT}.date.data.${i} > ${PM_INFO_WATT}-date-data-${i}.csv
-    cat header2.txt ${PM_INFO_WATT}.data.${i} > ${PM_INFO_WATT}-data-${i}.csv
-    rm header1.txt header2.txt \
-      ${PM_INFO_WATT}.date.data.${i} ${PM_INFO_WATT}.data.${i} \
-      screenlog.0 data.tmp pmlib_0.log stxxl.errlog stxxl.log
+    ###################################3
+    sleep ${SLEEP_FINISH}s
+    ssh debian@10.209.2.79 "screen -X -S ${PMLIB_WATT} quit"  &> /dev/null
+    screen -X -S ${PM_INFO_WATT} quit &> /dev/null
+  done
 done
 
+cp metrics.log ${DIRECTORY}
+cp ~/git/scripts/metrics.sh ${DIRECTORY}
+
+# ~/git/python-scripts/.venv/bin/python ~/git/python-scripts/transform_timestamp.py -d ${DIRECTORY}
+# ~/git/plots/.venv/bin/python ~/git/plots/wattios.py -d ${DIRECTORY}
 
 #############################################################################################################
 
-# TODO Update as watt measurement or make a loop for watt, amp, volt mesurements
-
-# echo "**************** ${PMLIB_VOLT}"
-# screen -dmS ${PMLIB_VOLT} /home/debian/bin/pmlib_server --configfile git/PMLib/new/${CAPEV}
-# screen -h 1000000000 -L -dmS ${PM_INFO_VOLT} /home/debian/bin/pm-info -s localhost:6526 -r APCape8L
-# 
-# for i in $(seq 1 10);
-# do
-#     echo "Run: ${i}"
-#     sleep 20s
-#     START=$(date +%Y-%m-%d\ %H:%M:%S\ %N)
-#     time ssh ${REMOTE_USER}@${HOST} "/opt/jdk1.8.0_202/jre/bin/java -jar /home/odroid/research.matrix.sequential-all-0.2.jar 1000 50 0"
-#     echo "Start matrix multiplication: ${START}"
-#     echo "Finish matrix multiplication: $(date +%Y-%m-%d\ %H:%M:%S\ %N)"
-# done
-# 
-# screen -X -S ${PM_INFO_VOLT} quit
-# screen -X -S ${PMLIB_VOLT} quit
-# awk '{print $1":"$2":"$3}' screenlog.0 | awk -F ':' '{print $1":"$2":"$3","$5}' > ${PM_INFO_VOLT}.date.data
-# awk '{print $1":"$2":"$3}' screenlog.0 | awk -F ':' '{print $5}' > ${PM_INFO_VOLT}.data
-# echo "Time,Voltage(Volt)" > header1.txt
-# echo "Voltage(Volt)" > header2.txt
-# cat header1.txt ${PM_INFO_VOLT}.date.data > ${PM_INFO_VOLT}-date-data.csv
-# cat header2.txt ${PM_INFO_VOLT}.data > ${PM_INFO_VOLT}-data.csv
-# rm header1.txt header2.txt ${PM_INFO_VOLT}.date.data ${PM_INFO_VOLT}.data screenlog.0
-
-
-#############################################################################################################
-# echo "**************** ${PMLIB_AMP}"
-# screen -dmS ${PMLIB_AMP} /home/debian/bin/pmlib_server --configfile git/PMLib/new/${CAPEA}
-# screen -h 1000000000 -L -dmS ${PM_INFO_AMP} /home/debian/bin/pm-info -s localhost:6526 -r APCape8L
-# 
-# for i in $(seq 1 10);
-# do
-#     echo "Run: ${i}"
-#     sleep 20s
-#     START=$(date +%Y-%m-%d\ %H:%M:%S\ %N)
-#     time ssh ${REMOTE_USER}@${HOST} "/opt/jdk1.8.0_202/jre/bin/java -jar /home/odroid/research.matrix.sequential-all-0.2.jar 1000 50 0"
-#     echo "Start matrix multiplication: ${START}"
-#     echo "Finish matrix multiplication: $(date +%Y-%m-%d\ %H:%M:%S\ %N)"
-# done
-
-# screen -X -S ${PM_INFO_AMP} quit
-# screen -X -S ${PMLIB_AMP} quit
-# awk '{print $1":"$2":"$3}' screenlog.0 | awk -F ':' '{print $1":"$2":"$3","$5}' > ${PM_INFO_AMP}.date.data
-# awk '{print $1":"$2":"$3}' screenlog.0 | awk -F ':' '{print $5}' > ${PM_INFO_AMP}.data
-# echo "Time,Current(mAmp)" > header1.txt
-# echo "Current(mAmp)" > header2.txt
-# cat header1.txt ${PM_INFO_AMP}.date.data > ${PM_INFO_AMP}-date-data.csv
-# cat header2.txt ${PM_INFO_AMP}.data > ${PM_INFO_AMP}-data.csv
-# rm header1.txt header2.txt ${PM_INFO_AMP}.date.data ${PM_INFO_AMP}.data screenlog.0
+# TODO Make a loops for amp & volt mesurements too
