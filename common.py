a88397b2801cd74db5cb6e68c721dddff7c035e2
Pavel Nichita <pnichita@ull.edu.es>
2019-07-05 09:31:28 +0100

    -Add OS parameter to the naming scheme

--- common.py
+++ common.py
@@ -13,9 +13,10 @@ CSV_OP = 'Operation'
 
 
 # Files naming scheme:
-# XXXXXXXXX_<device>_<bench>_<class>_<threads>[_<iteration>].<suffix>
+# XXXXXXXXX_<device>_<os>_<bench>_<class>_<threads>[_<iteration>].<suffix>
 # Where XXXXXXXXX is whatever, it must not contain '_' char
 # <device> is the name of the device, example: 'hikey970', 'odroidxu4a'...
+# <os> is the operating system, either 'android' or 'linux'
 # <bench> is the name of npb benchmark, usually has a length of 2 chars, examples: 'is', 'fe'...
 # <class> is the size of the benchmark, has a length of 1 char, examples: 's', 'w', 'c'...
 # <iteration> is used for energy data files (csv's) and denotes the number of iteration that was running
