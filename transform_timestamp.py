b6ea8e7ced43aa0d8d852f298c00617a9bb8d0d2
Pavel Nichita <pnichita@ull.edu.es>
2019-07-11 14:27:57 +0100

    -Moving functions around, no code change

--- transform_timestamp.py
+++ transform_timestamp.py
@@ -21,6 +21,41 @@ logging.basicConfig(
 logger = logging.getLogger('TRANSFORM_CSV')
 
 
+def csv_shortcuts(data):
+    data_time = data.get('time')
+    data_mw = data.get('mw')
+    data_op = data.get('op')
+    data_time_xs = data.get('time_xs')
+    data_time_00 = data.get('time_00')
+    data_us = data.get('us')
+
+    return data_time, data_mw, data_op, data_time_xs, data_time_00, data_us
+
+
+# TODO a line can contain NULL byte, this script does not control this use case
+def pre_compute_checks(file):
+    """
+    Checks in file:
+    1. return the first timestamp that appears in file
+    2. checks if last row is valid (complete data), if no deletes it
+    3. searches and returns timestamp of XS and XF marks
+    :param file: csv file with raw data
+    :return: xs, xf and first timestamps
+    """
+    ts_first = first_timestamp(file)
+    check_last_row(file)
+    ts_xs, ts_xf = None, None
+    reader = csv.DictReader(file)
+    for row in reader:
+        op = row.get(CSV_OP)
+        if op == 'XS':
+            ts_xs = read_timestamp(row.get(CSV_TIME))
+        if op == 'XF':
+            ts_xf = read_timestamp(row.get(CSV_TIME))
+
+    return ts_xs, ts_xf, ts_first
+
+
 def log_to_file():
     file_log = logging.FileHandler('transform_csv.log', mode='w')
     file_log.setLevel(logging.INFO)
@@ -48,25 +83,6 @@ def profile(m, function, *args):
     return ret
 
 
-def backwards_xs_time_compute(data_time, ts_xs):
-    time_xs = []
-    for ts in data_time:
-        time_xs.append(
-            (ts - ts_xs).total_seconds()
-        )
-    return time_xs
-
-
-def csv_shortcuts(data):
-    data_time = data.get('time')
-    data_mw = data.get('mw')
-    data_op = data.get('op')
-    data_time_xs = data.get('time_xs')
-    data_time_00 = data.get('time_00')
-    data_us = data.get('us')
-    return data_time, data_mw, data_op, data_time_xs, data_time_00, data_us
-
-
 def write_csv_dict_with_lists(filename, csv_data):
     with open(filename, 'w') as f:
         header = list(csv_data.keys())
@@ -91,75 +107,24 @@ def write_csv_list_of_dict(filename, csv_data):
         logger.warning('[NO DATA IN ENERGY CSV]')
 
 
-def parse_args():
-    # Parsear linea de comandos
-    parser = OptionParser("usage: %prog -d|--directory DIRECTORY")
-    parser.add_option("-d", "--directory", action="store", type="string", dest="directory")
-    parser.add_option("-c", "--cores", action="store", type="int", dest="cores")
-    (options, args) = parser.parse_args()
-    if not options.directory:
-        logger.error('[You must specify a working directory]')
-        parser.print_help()
-        sys.exit(-1)
-    options.cores = set_cores(options.cores)
-    return options
-
-
-def get_files():
+def calculate_energy(power, power_prev, us):
     """
-    Returns a list of files reverse sorted by size.
-    Bigger files are first processed to try and maximize the efficiency.
-    This does not guaranty that bigger files will always take more time
-    to process then smaller files.
-    :return: a list of string representing files in current directory
+    Calculate energy in joules for the zone defined by Xn, X(n-1) and { [Tn - T(n-1)] => us }
+    :param power: Xn (current power)
+    :param power_prev: X(n-1) (previous power)
+    :param us: Tn-T(n-1) in microseconds
+    :return: float, computed energy in joules
     """
-    files = os.listdir(os.curdir)
-    size_file = []
-    for filename in files:
-        if filename.startswith('data') and filename.endswith('.csv'):
-            if f'transformed-{filename}' not in files:
-                size = os.path.getsize(filename)
-                size_file.append((size, filename))
-    size_file.sort(key=lambda s: s[0], reverse=True)
-    return [file[1] for file in size_file]
+    return (float(power) + float(power_prev)) / 2 * us
 
 
-def main(energy_csv):
-    options = parse_args()
-    os.chdir(options.directory)
-    logger.addHandler(log_to_file())
-
-    cwd = os.getcwd()
-    files = get_files()
-    # TODO mem profiling not working with mp
-    mem.append(f'Default memory: {memory_usage()}MB')
-    # TODO a more dynamic way to assign cores
-    with Pool(options.cores) as p:
-        results = [p.apply_async(file_compute, (cwd, file)) for file in files]
-        for result in results:
-            energy_csv.append(result.get())
-
-
-def file_compute(cwd, file):
-    logger.info(f'[{cwd}][{file}]')
-    mem.append(f'***************************** {file} *****************************')
-    data = {'time_str': [], 'time': [], 'mw': [], 'op': [], 'time_xs': [], 'time_00': [], 'us': []}
-    ts_xs = None
-    ts_xf = None
-    with open(file, 'r+') as f:
-        energy_dict = csv_name_parsing(file)
-        ts_first = profile(mem, first_timestamp, f)
-        profile(mem, check_last_row, f)
-        ts_xs, ts_xf, energy_dict['joules'], energy_dict['time'] = \
-            profile(mem, csv_compute, data, f, ts_first, ts_xf, ts_xs)
-    if ts_xs and ts_xf:
-        # write_csv(file, data, mem)
-        del data['time']
-        profile(mem, write_csv_dict_with_lists, f'transformed-{file}', data)
-    else:
-        logger.warning(f'[{cwd}][{file}][XS operation not found, skip this file]')
-        energy_dict['joules'], energy_dict['time'] = '', ''
-    return energy_dict
+def backwards_xs_time_compute(data_time, ts_xs):
+    time_xs = []
+    for ts in data_time:
+        time_xs.append(
+            (ts - ts_xs).total_seconds()
+        )
+    return time_xs
 
 
 def csv_compute(data, file, ts_first, ts_xf, ts_xs):
@@ -202,39 +167,75 @@ def csv_compute(data, file, ts_first, ts_xf, ts_xs):
     return ts_xs, ts_xf, energy / 1000000000, time_us / 1000000
 
 
-def calculate_energy(power, power_prev, us):
-    """
-    Calculate energy in joules for the zone defined by Xn, X(n-1) and { [Tn - T(n-1)] => us }
-    :param power: Xn (current power)
-    :param power_prev: X(n-1) (previous power)
-    :param us: Tn-T(n-1) in microseconds
-    :return: float, computed energy in joules
-    """
-    return (float(power) + float(power_prev)) / 2 * us
+def file_compute(cwd, file):
+    logger.info(f'[{cwd}][{file}]')
+    mem.append(f'***************************** {file} *****************************')
+    data = {'time_str': [], 'time': [], 'mw': [], 'op': [], 'time_xs': [], 'time_00': [], 'us': []}
+    ts_xs = None
+    ts_xf = None
+    with open(file, 'r+') as f:
+        energy_dict = csv_name_parsing(file)
+        ts_first = profile(mem, first_timestamp, f)
+        profile(mem, check_last_row, f)
+        ts_xs, ts_xf, energy_dict['joules'], energy_dict['time'] = \
+            profile(mem, csv_compute, data, f, ts_first, ts_xf, ts_xs)
+    if ts_xs and ts_xf:
+        # write_csv(file, data, mem)
+        del data['time']
+        profile(mem, write_csv_dict_with_lists, f'transformed-{file}', data)
+    else:
+        logger.warning(f'[{cwd}][{file}][XS operation not found, skip this file]')
+        energy_dict['joules'], energy_dict['time'] = '', ''
+    return energy_dict
 
 
-# TODO a line can contain NULL byte, this script does not control this use case
-def pre_compute_checks(file):
+def get_files():
     """
-    Checks in file:
-    1. return the first timestamp that appears in file
-    2. checks if last row is valid (complete data), if no deletes it
-    3. searches and returns timestamp of XS and XF marks
-    :param file: csv file with raw data
-    :return: xs, xf and first timestamps
+    Returns a list of files reverse sorted by size.
+    Bigger files are first processed to try and maximize the efficiency.
+    This does not guaranty that bigger files will always take more time
+    to process then smaller files.
+    :return: a list of string representing files in current directory
     """
-    ts_first = first_timestamp(file)
-    check_last_row(file)
-    ts_xs, ts_xf = None, None
-    reader = csv.DictReader(file)
-    for row in reader:
-        op = row.get(CSV_OP)
-        if op == 'XS':
-            ts_xs = read_timestamp(row.get(CSV_TIME))
-        if op == 'XF':
-            ts_xf = read_timestamp(row.get(CSV_TIME))
+    files = os.listdir(os.curdir)
+    size_file = []
+    for filename in files:
+        if filename.startswith('data') and filename.endswith('.csv'):
+            if f'transformed-{filename}' not in files:
+                size = os.path.getsize(filename)
+                size_file.append((size, filename))
+    size_file.sort(key=lambda s: s[0], reverse=True)
+    return [file[1] for file in size_file]
 
-    return ts_xs, ts_xf, ts_first
+
+def parse_args():
+    # Parsear linea de comandos
+    parser = OptionParser("usage: %prog -d|--directory DIRECTORY")
+    parser.add_option("-d", "--directory", action="store", type="string", dest="directory")
+    parser.add_option("-c", "--cores", action="store", type="int", dest="cores")
+    (options, args) = parser.parse_args()
+    if not options.directory:
+        logger.error('[You must specify a working directory]')
+        parser.print_help()
+        sys.exit(-1)
+    options.cores = set_cores(options.cores)
+    return options
+
+
+def main(energy_csv):
+    options = parse_args()
+    os.chdir(options.directory)
+    logger.addHandler(log_to_file())
+
+    cwd = os.getcwd()
+    files = get_files()
+    # TODO mem profiling not working with mp
+    mem.append(f'Default memory: {memory_usage()}MB')
+    # TODO a more dynamic way to assign cores
+    with Pool(options.cores) as p:
+        results = [p.apply_async(file_compute, (cwd, file)) for file in files]
+        for result in results:
+            energy_csv.append(result.get())
 
 
 if __name__ == "__main__":
