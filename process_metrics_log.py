cd274e2a48f4591adcc4c558b6d96d60315ea690
Pavel Nichita <pnichita@ull.edu.es>
2019-07-17 15:03:55 +0100

    -A somewhat better metrics*.log processing

--- process_metrics_log.py
+++ process_metrics_log.py
@@ -12,7 +12,14 @@ logging.basicConfig(
 )
 
 
-def metrics_compute(cwd, file):
+def metrics_file_process(cwd: str, file: str) -> [dict]:
+    """
+    Opens 'file', reads all it's lines and collects data with each
+    run of a benchmark in a dict.
+    :param cwd: string, current working directory
+    :param file: string, name of file to process
+    :return: a list of dict
+    """
     logger.info(f'[{cwd}][{file}]')
     data = []
     name_parsed = csv_name_parsing(file)
@@ -21,37 +28,52 @@ def metrics_compute(cwd, file):
         i = 0
         while i < len(lines):
             if 'Run:' in lines[i]:
-                metric_dict = {}
-                metric_dict.update(name_parsed)
-                iteration = int(lines[i].split()[2])
-                metric_dict.update({'iteration': f'{iteration:03}'})
-                time_npb = ''
-                mops = ''
-                i += 1
-                try:
-                    while 'Run:' not in lines[i]:
-                        if 'Time in ' in lines[i]:
-                            time_npb = float(lines[i].split()[5])
-                        elif 'Mops ' in lines[i]:
-                            mops = float(lines[i].split()[4])
-                        i += 1
-                    i -= 1
-                except IndexError:
-                    pass
-                metric_dict.update({'time_npb': time_npb, 'mops': mops})
+                i, metric_dict = block_process(i, lines, name_parsed)
                 data.append(metric_dict)
-
             i += 1
     return data
 
 
+def block_process(i: int, lines: list, name_parsed: dict) -> 'int,dict':
+    """
+    Process data between lines with 'Run: n' and 'Run: n+1'.
+    This function has a sketchy implementation and philosophy in general,
+    main candidate to refactor.
+    :param i: int current position in 'lines'
+    :param lines: list of strings with all lines from the file
+    :param name_parsed: a dict with key and value predefined
+    :return: int i: position for previous line before 'Run: n+1', dict
+    """
+    metric_dict = name_parsed.copy()
+    iteration = int(lines[i].split()[2])
+    time_npb = ''
+    mops = ''
+    # Let go to the next line, it should have 'Size: x'
+    i += 1
+    # try/except block for when we get to the appearance of last 'Run: x'
+    # since after this block there will be no more of 'Run: x' it will raise
+    # an IndexError. No more available lines to process.
+    try:
+        while 'Run:' not in lines[i]:
+            if 'Time in ' in lines[i]:
+                time_npb = float(lines[i].split()[5])
+            elif 'Mops ' in lines[i]:
+                mops = float(lines[i].split()[4])
+            # While in 'Run: n' block we must go to the next line
+            i += 1
+        # We are at the line with 'Run: n+1', we must set the position to the previous
+        # line so that outer function can read it and launch this function again
+        i -= 1
+    except IndexError:
+        pass
+    metric_dict.update({'iteration': f'{iteration:03}', 'time_npb': time_npb, 'mops': mops})
+    return i, metric_dict
+
+
 def get_files():
     """
-    Returns a list of files reverse sorted by size.
-    Bigger files are first processed to try and maximize the efficiency.
-    This does not guaranty that bigger files will always take more time
-    to process then smaller files.
-    :return: a list of string representing files in current directory
+    Filter files from current directory.
+    :return: a list of string, each string is a file name from current directory
     """
     files = os.listdir(os.curdir)
     files_ret = []
@@ -70,10 +92,8 @@ def main():
     files = get_files()
     processed_data = []
 
-
-    # TODO mem profiling not working with mp
     with Pool(options.cores) as p:
-        results = [p.apply_async(metrics_compute, (cwd, file)) for file in files]
+        results = [p.apply_async(metrics_file_process, (cwd, file)) for file in files]
         for result in results:
             processed_data.extend(result.get())
 
