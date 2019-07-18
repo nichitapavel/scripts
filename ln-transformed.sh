0c696889c079a3b7c05458d73c9d1c310a5ed7f6
Pavel Nichita <pnichita@ull.edu.es>
2019-07-18 15:24:05 +0100

    -Improvend symbolic link creator

--- ln-transformed.sh
+++ ln-transformed.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-# set -x
+# set -xe
 
-FOLDER=$1
-FILES=$(echo $(find ${FOLDER} -type f | grep transformed))
+FOLDER="$2"
+TYPE="$1"
+if [ "${TYPE}" == "metrics" ]; then
+    FILES=$(echo $(find ${FOLDER} -type f | grep metrics))
+elif [ "${TYPE}" == "data" ]; then
+    FILES=$(echo $(find ${FOLDER} -type f | grep -v transformed| grep -v log | grep -v png | grep data | grep csv))
+else
+    echo "Unkown option. Exiting..."
+    exit 1
+fi
+
 IFS=' ' read -r -a LINES <<< ${FILES}
 
 for line in ${LINES[@]};
 do
-    NUMBER=$(echo ${line} | awk -F '/' '{print $4}')
-    NAME=$(echo ${line} | awk -F '/' '{print $7}')
-    ln -s ${line} ${NUMBER}-${NAME}
+    NUMBER=$(echo ${line} | awk -F '/' '{print $6}')
+    TYPE=$(echo ${line} | awk -F '/' '{print $4}')
+    NAME=$(echo ${line} | awk -F '/' '{print $8}')
+    ln -s ${line} ${NUMBER}_${TYPE}_${NAME}
+    # echo ${line} ${NUMBER}_${TYPE}_${NAME}
 done
